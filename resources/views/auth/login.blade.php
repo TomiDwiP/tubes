cf880b671c96ca66cbcfb6e4583a3690e7a71ab3
TomiDwiP <tomidp98@gmail.com>
2018-05-13 23:02:42 +0700

    coba

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="nim" class="col-sm-4 col-form-label text-md-right">{{ __('Nim') }}</label>
+                            <label for="nim" class="col-sm-4 col-form-label text-md-right">{{ __('NIM') }}</label>
 
                             <div class="col-md-6">
                                 <input id="nim" type="text" class="form-control{{ $errors->has('nim') ? ' is-invalid' : '' }}" name="nim" value="{{ old('nim') }}" required autofocus>
